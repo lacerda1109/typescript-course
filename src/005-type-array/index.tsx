fca6d92fea0f8d656882d5c71f4f4ffe892565e9
Gabriel Lacerda <devlacerda1109@gmail.com>
2023-02-01 13:07:34 -0300

    Add more in lesson 005

--- src/005-type-array/index.tsx
+++ src/005-type-array/index.tsx
@@ -14,3 +14,7 @@ function toUpperCase(...args: string[]): string[] {
   return args.map((value) => value.toUpperCase());
 }
 // console.log(toUpperCase("a", "b", "c"));
+
+// Para arrays imutáveis, usamos readonly
+const arr1: readonly string[] = ["a", "b", "c"];
+const arr2: ReadonlyArray<string> = ["a", "b", "c"];
