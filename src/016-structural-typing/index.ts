a7ca739d733da092b8d284295abbf565511629c5
Gabriel Lacerda <devlacerda1109@gmail.com>
2023-02-13 11:39:55 -0300

    Add more to lesson 016

--- src/016-structural-typing/index.ts
+++ src/016-structural-typing/index.ts
@@ -16,13 +16,21 @@ const verifyUser: VerifyUserFn = (user, sentValue) => {
 };
 
 const bdUser = { username: "joao", password: "123456" };
-const sentUser = { username: "joao", password: "123456" };
+const sentUser = { username: "joao", password: "123456", permissions: "" };
 
 /**
  * Com isso, abaixo o TypeScript se importa apenas se os tipos são equivalentes,
  * como é o caso dos parâmetros passados, onde "bdUser" e "sentUser" não são
- * explicitamente do tipo "User".
+ * explicitamente do tipo "User", o que nos permite por exemplo ter a chave
+ * "permissions" em um deles.
  */
 const loggedIn = verifyUser(bdUser, sentUser);
 
 console.log(loggedIn);
+
+/**
+ * "Um sistema de tipo estrutural é uma classe principal de sistemas de tipo em que
+ * a compatibilidade e a equivalência de tipo são determinadas pela estrutura ou
+ * definição real do tipo e não por outras características, como seu nome ou local
+ * de declaração." - Wikipédia
+ */
