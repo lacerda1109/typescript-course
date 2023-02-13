5cd5f6a7d0b1722879776b0346a9cbf1e86567e7
Gabriel Lacerda <devlacerda1109@gmail.com>
2023-02-13 12:54:38 -0300

    Add more to lesson 017

--- src/017-type-assertions/index.ts
+++ src/017-type-assertions/index.ts
@@ -4,6 +4,9 @@
  * Como se fosse uma conversão de um tipo para outro. Usamos de diferentes formas
  * para dizer ao TypeScript que sabemos que algo realmente existe (não tendo a
  * possibilidade de ser null, por exemplo).
+ * 
+ * Podemos dizer que é usado para verificar uma condição que o desenvolvedor supõe
+ * que seja verdadeira em determinado ponto.
  */
 
 // Conditional
@@ -11,7 +14,7 @@ const body1 = document.querySelector("body");
 if (body1) body1.style.background = "red";
 
 // Non-null assertion (!)
-const body2 = document.querySelector("body")!;
+const body2 = document.querySelector("body")!; // O exclamação indica que o objeto não pode ser nulo.
 body2.style.background = "red";
 
 // Type assertion
